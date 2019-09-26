0085f781c147a294fcbfabb52103bbe041cec3bc
Jmcclain0129 <jmcclain0129@gmail.com>
2019-09-26 09:27:16 +0000

    made migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-# import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "No Secret key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["79b91c06532741a386dcb141e0aecdff.vfs.cloud9.us-east-1.amazonaws.com", "alpha-beta-charlie.herokuapp.com", 'localhost', '8000-b700bef5-4e4d-4f49-86f6-78800e9dc5c4.ws-ap0.gitpod.io']
+ALLOWED_HOSTS = ['79b91c06532741a386dcb141e0aecdff.vfs.cloud9.us-east-1.amazonaws.com', 'alpha-beta-charlie.herokuapp.com', 'localhost', '8000-b700bef5-4e4d-4f49-86f6-78800e9dc5c4.ws-ap0.gitpod.io']
 # [os.environ.get('C9_HOSTNAME')]
 
 # Application definition
