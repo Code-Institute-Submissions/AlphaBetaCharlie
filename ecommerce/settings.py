9eb3100502bd9180cb87593646a49f306916b76f
John McClain <jmcclain0129@gmail.com>
2019-10-10 13:32:34 +0100

    first commit from vs code ide

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-# import env
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,13 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', "No Secret key")
-SECRET_KEY = os.getenv('SECRET_KEY', "No Secret key")
+SECRET_KEY = os.environ.get('SECRET_KEY', "No Secret key")
+#SECRET_KEY = os.getenv('SECRET_KEY', "No Secret key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['79b91c06532741a386dcb141e0aecdff.vfs.cloud9.us-east-1.amazonaws.com', 'alpha-beta-charlie.herokuapp.com', 'localhost', '8000-b700bef5-4e4d-4f49-86f6-78800e9dc5c4.ws-ap0.gitpod.io']
+ALLOWED_HOSTS = ['79b91c06532741a386dcb141e0aecdff.vfs.cloud9.us-east-1.amazonaws.com', 'alpha-beta-charlie.herokuapp.com', 'localhost', '8000-b700bef5-4e4d-4f49-86f6-78800e9dc5c4.ws-ap0.gitpod.io', '127.0.0.1']
 # [os.environ.get('C9_HOSTNAME')]
 
 # Application definition
