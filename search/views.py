034dbb45ae19934bb4a1ccffa4fe88fed8b31abd
John McClain <jmcclain0129@gmail.com>
2019-10-10 19:39:36 +0100

    added comma after url line 8

--- search/views.py
+++ search/views.py
@@ -4,5 +4,5 @@ from products.models import Product, ProductBase, ProductType
 # Create your views here.
 
 def do_search(request):
-    products = ProductBase.objects.filter(name__icontains=request.GET['q'])
-    return render(request, "products_user.html" {"products":products})
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products_user.html", {"products":products})
