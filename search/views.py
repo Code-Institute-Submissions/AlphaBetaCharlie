72406dcb898fa7680ebf0a49cb71f0ba5f371052
John McClain <jmcclain0129@gmail.com>
2019-10-11 00:17:31 +0100

    updated name to username in search/views.py line7 and templates/base.py line67

--- search/views.py
+++ search/views.py
@@ -4,5 +4,5 @@ from products.models import Product, ProductBase, ProductType
 # Create your views here.
 
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(username__icontains=request.GET['q'])
     return render(request, "products_user.html", {"products":products})
