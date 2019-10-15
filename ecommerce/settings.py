fbb046738f260990f74cc01931febb574ea7d3db
John McClain <jmcclain0129@gmail.com>
2019-10-15 17:16:08 +0100

    added additional template in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'accounts.contexts.transaction_summary',
                 'cart.contexts.cart_contents'
             ],
         },
